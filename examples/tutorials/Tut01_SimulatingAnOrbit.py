aadf651409b82b9144abc1e03e6175037df7196a
William Easdown <34220924+weasdown@users.noreply.github.com>
2023-12-18 12:22:18 +0000

    Added CreateDefault for StopCondition

--- examples/tutorials/Tut01_SimulatingAnOrbit.py
+++ examples/tutorials/Tut01_SimulatingAnOrbit.py
@@ -55,7 +55,7 @@ sat_params = {
 }
 
 # sat = gpy.Spacecraft.from_dict(sat_params)
-sat = gpy.Spacecraft('SatOne')
+sat = gpy.Spacecraft('Sat')
 print(f'state: {sat.GetState()}')
 # sat.SetField('DateFormat', 'UTCGregorian')
 
@@ -195,7 +195,30 @@ bms.Initialize()
 
 # Create a Propagate command
 
-pgate = mod.CreateDefaultCommand('Propagate', 'Pgate')
+pgate: gmat.GmatCommand = gpy.Propagate.CreateDefault()
+print(f'\n{pgate}, {type(pgate)}')
+ty_ar = pgate.GetRefObjectTypeArray()
+for num in ty_ar:
+    print(f'Object type ID: {num}')
+    print(gmat.ShowObjectsForID(num))
+
+# pgate.Help()
+
+# gmat.ShowObjects()
+a1m = gmat.GetObject(f'{sat.name}.A1ModJulian')
+es = gmat.GetObject(f'{sat.name}.ElapsedSecs')
+dp = gmat.GetObject(f'DefaultProp')
+dp_fm = gmat.GetObject(f'DefaultProp_ForceModel')
+
+# print(f'{a1m}, {type(a1m)}, {a1m.GetTypeName()}')
+# print(f'{es}, {type(es)}, {es.GetTypeName()}')
+# print(f'{dp}, {type(dp)}, {dp.GetTypeName()}')
+# print(f'{dp_fm}, {type(dp_fm)}, {dp_fm.GetTypeName()}')
+
+# a1m.Help()
+# es.Help()
+# dp.Help()
+# dp_fm.Help()
 
 # Get info about startup file settings, to debug log error #  "Error occurred during initialization: Utility Exception:
 # FileManager::ReadStartupFile() cannot open GMAT startup file: "C:\Users\[name]\AppData\Local\Programs\Python\
--- gmat_py_simple/executive.py
+++ gmat_py_simple/executive.py
@@ -12,6 +12,13 @@ class Moderator:
     def Initialize(self):
         self.gmat_obj.Initialize()
 
+    def GetDefaultSpacecraft(self) -> gmat.Spacecraft:
+        so_config_list: list[str] = self.gmat_obj.GetListOfObjects(gmat.SPACECRAFT)
+        if so_config_list:  # list length > 0
+            return self.gmat_obj.GetSpacecraft(so_config_list[0])
+        else:  # no spacecraft found, so create one
+            return self.gmat_obj.CreateSpacecraft('Spacecraft', 'DefaultSC')
+
     def GetRunState(self):
         rs = self.gmat_obj.GetRunState()
         if rs == 10000:
@@ -55,3 +62,36 @@ class Moderator:
 
     def RunMission(self):
         return self.gmat_obj.RunMission()
+
+    def GetListOfObjects(self, obj_type: int, exclude_defaults: bool = False, type_max: int = 0):
+        return self.gmat_obj.GetListOfObjects(obj_type, exclude_defaults, type_max)
+
+    def CreateDefaultStopCondition(self) -> gmat.StopCondition:
+        """
+
+        :return:
+        """
+
+        sc: gmat.Spacecraft = self.GetDefaultSpacecraft()
+        sc_name: str = sc.GetName()
+        epoch_var = f'{sc_name}.A1ModJulian'
+        stop_var = f'{sc_name}.ElapsedSecs'
+
+        mod = Moderator()
+        if not mod.GetParameter(epoch_var):
+            param: gmat.Parameter = mod.gmat_obj.CreateParameter('A1ModJulian', epoch_var)
+            param.SetRefObjectName(gmat.SPACECRAFT, sc_name)
+
+        if not mod.GetParameter(stop_var):
+            param: gmat.Parameter = mod.gmat_obj.CreateParameter('ElapsedSecs', stop_var)
+            param.SetRefObjectName(gmat.SPACECRAFT, sc_name)
+
+        stop_cond_name = f'StopOn{stop_var}'
+        stop_cond: gmat.StopCondition = mod.gmat_obj.CreateStopCondition('StopCondition', stop_cond_name)
+        stop_cond.SetStringParameter('EpochVar', epoch_var)
+        stop_cond.SetStringParameter('StopVar', stop_var)
+        stop_cond.SetStringParameter('Goal', '12000.0')
+        return stop_cond
+
+    def GetParameter(self, param: str) -> gmat.Parameter:
+        return self.gmat_obj.GetParameter(param)
