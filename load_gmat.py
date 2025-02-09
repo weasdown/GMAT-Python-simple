aed524222f8174074b7803f7fd1629c681f4edcf
weasdown <34220924+weasdown@users.noreply.github.com>
2025-02-09 20:08:05 +0000

    feat: make load_gmat.py generic

--- load_gmat.py
+++ load_gmat.py
@@ -9,21 +9,24 @@ import sys
 from os import path
 
 apistartup = "api_startup_file.txt"
-GmatInstall = "C:\\Users\\weasd\\Desktop\\GMAT\\gmat-win-R2022a\\GMAT"
+
+# Absolute path to your root GMAT folder (below is just an example).
+# On Windows, be sure to use either double backslashes or single forward slashes.
+GmatInstall = "C:\\Users\\[USERNAME]\\dev\\GMAT\\gmat-win-R2022a\\GMAT"
+
 GmatBinPath = GmatInstall + "/bin"
 Startup = GmatBinPath + "/" + apistartup
 
 if path.exists(Startup):
-    print(f'Running GMAT in {GmatInstall}')
 
-    sys.path.insert(1, GmatBinPath)
 
-    import gmatpy as gmat
+   sys.path.insert(1, GmatBinPath)
 
-    gmat.Setup(Startup)
+   import gmatpy as gmat
+   gmat.Setup(Startup)
 
 else:
-    print("Cannot find ", Startup)
-    print()
-    print("Please set up a GMAT startup file named ", apistartup, " in the ",
-          GmatBinPath, " folder.")
+   print("Cannot find ", Startup)
+   print()
+   print("Please set up a GMAT startup file named ", apistartup, " in the ", 
+      GmatBinPath, " folder.")
